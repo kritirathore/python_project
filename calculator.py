6bcfccdd4c1c746f0b417dd768c1a4dd3b2c172c
Kriti <krattyrocks@gmail.com>
2018-05-22 23:22:20 +0530

    Program for Simple Calculator

--- calculator.py
+++ calculator.py
@@ -1,31 +1,36 @@
-print("Select Operation");
-print("1. Add");
-print("2. Subtract");
-print("3. Multiply");
-print("4. Divide");
+def add(x, y):
+   return x + y
+ 
+def subtract(x, y):
+   return x - y
 
-num1 = int(input("Enter Number 1:"));
-num2 = int(input("Enter Number 2:"));
+def multiply(x, y):
+   return x * y
 
-x = input("enter choice 1,2,3,4: ");
+def divide(x, y):
+   return x / y
 
-if x == '1':
-		result=num1+num2;
-		print(result);
-		
-elif x == '2':
-		result=num1-num2;
-		print(result);
-elif x == '3':
-		result=num1*num2;
-		print(result);	
-elif x == '4' :
-		result=num1/num2;
-		print(result);		
-else :
-		print("invalid input");
-		
-		
-		
-word = "Python";
-print(word[0:2]);
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("Enter choice(1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
